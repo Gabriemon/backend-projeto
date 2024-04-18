9547b00a1ee781d81310d78b55b7f1950c501b0f
Your Name <you@example.com>
2024-04-18 18:21:23 -0300

    Processo de desenvolvimento

--- jogos/models.py
+++ jogos/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Jogos(models.Model):
 
     nome = models.CharField(max_length=30)
-    data_lancamento = models.CharField(max_length=9)
+    data_lancamento = models.CharField(max_length=50)
     preco = models.CharField(max_length=11)
 
     def __str__(self):
